5b45537a40f0bfa77d8903d29005f6e91a2c61e3
Bhavin0608 <bhavinmakwana2006@gmail.com>
2026-03-23 12:24:52 +0530

    [ADD] Booking Entity

---
+++ src/main/java/com/cfbmapi/entity/Booking.java
@@ -0,0 +1,50 @@
+package com.cfbmapi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "bookings")
+@Data
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private int id;
+
+    @Column(nullable = false)
+    private LocalDate bookingDate;
+
+    @Column(nullable = false)
+    private LocalTime startTime;
+
+    @Column(nullable = false)
+    private LocalTime endTime;
+
+    @Column(nullable = false)
+    private String purpose;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BookingStatus status = BookingStatus.PENDING;
+
+    @Column(nullable = false, name = "user_id")
+    private int userId;
+
+    @Column(nullable = false, name = "facility_id")
+    private int facilityId;
+
+    @Column(nullable = false)
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void setCreatedAt(){
+        this.createdAt = LocalDateTime.now();
+    }
+}
